7d6b58a1b191f4a2b0b221fe334fa2305d7c69f9
blahablast <blahasam@gmail.com>
2023-01-25 18:07:47 -0500

    Initial Commit

---
+++ slots/slotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  slotsApp.swift
+//  slots
+//
+//  Created by sam blaha on 1/25/23.
+//
+
+import SwiftUI
+
+@main
+struct slotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
